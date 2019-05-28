d468ef5f911d929769394a228692f402cf4dc777
Etienne Faisant <etienne.faisant@fime.com>
2019-05-28 16:00:02 +0200

    es6

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,4 +1,9 @@
-/* global suite, test, beforeEach*/
+/* global suite, test */
+import { strictEqual } from 'assert';
+import { beforeEach } from 'mocha';
+
+import { WordCounter } from '../src/wordCounter';
+
 
 //
 // Note: This example test is leveraging the Mocha test framework.
@@ -6,45 +11,47 @@
 //
 
 // The module 'assert' provides assertion methods from node
-var assert = require('assert')
-var beforeEach = require('mocha').beforeEach;
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-var vscode = require('vscode')
-var myExtension = require('../extension')
-var wordCounter = require('../wordCounter');
 // Defines a Mocha test suite to group tests of similar kind together
 suite('Wordcounter.wordCount()', function () {
-  var counter;
-  beforeEach(function() {
-    counter = new wordCounter.WordCounter();
+  var counter = null;
+
+  beforeEach(function () {
+    counter = new WordCounter();
     counter.count_words = true;
   });
+
   // Defines a Mocha unit test
   test('latin', function () {
-    assert.equal(counter.wordCount(`
+    strictEqual(counter.wordCount(`
     one two three
-    four five 
+    four five
     `), 5);
   });
+
   test('cyrillic', function () {
-    assert.equal(counter.wordCount(`
+    strictEqual(counter.wordCount(`
       один два три
       четыре Пять Ёлка
     `), 6);
   });
+
   test('mixed', function () {
-    assert.equal(counter.wordCount(`
+    strictEqual(counter.wordCount(`
       один two три
       four Пять ёлка
     `), 6);
   });
-  test('hyphenated', function() {
-    assert.equal(counter.wordCount('test-hyphen'), 1);
-    assert.equal(counter.wordCount('что-то'), 1);
+
+  test('hyphenated', function () {
+    strictEqual(counter.wordCount('test-hyphen'), 1);
+    strictEqual(counter.wordCount('что-то'), 1);
   });
-  test('contracted', function() {
-    assert.equal(counter.wordCount(`I won't`), 2);
-    assert.equal(counter.wordCount(`д'Артаньян`), 1);
+
+  test('contracted', function () {
+    strictEqual(counter.wordCount(`I won't`), 2);
+    strictEqual(counter.wordCount(`д'Артаньян`), 1);
   });
-})
+
+});
